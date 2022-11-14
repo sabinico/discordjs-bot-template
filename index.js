57fc1c6e0d18cf5f70980553231030ad6df0db61
Sabinico <sabinico@gmail.com>
2022-11-14 20:21:23 +0100

    Command execution in interactions

--- index.js
+++ index.js
@@ -32,9 +32,23 @@ client.once(Events.ClientReady, c => {
 });
 
 // Interactions
-client.on(Events.InteractionCreate, interaction => {
+client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 	console.log(interaction);
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
 // Log in to Discord with your client's token
